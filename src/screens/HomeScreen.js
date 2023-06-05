61e26f478279009bc52ec5610e826b9b7be6d89c
warzywko13 <adammen201300@gmail.com>
2023-06-05 19:02:21 +0200

    init

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <View className="bg-dark"><Text>HomeScreen</Text></View>
+  )
+}
+
+export default HomeScreen;
